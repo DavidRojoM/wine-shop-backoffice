88f0118fbeccb6133171867829923eedfdd65081
David Rojo <davidrojo1997@gmail.com>
2021-10-29 16:26:27 +0200

    first setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WineItemComponent } from './components/wine-item/wine-item.component';
+import { WineListComponent } from './components/wine-list/wine-list.component';
+import { WineControlsComponent } from './components/wine-controls/wine-controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WineItemComponent,
+    WineListComponent,
+    WineControlsComponent
   ],
   imports: [
     BrowserModule
